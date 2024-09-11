2469ef36311b58818a4f1eccfac359594fc12a22
William Hammond <william.t.hammond@gmail.com>
2024-09-10 22:48:29 -0400

    Simplifies build.bat even more

--- build.bat
+++ build.bat
@@ -17,15 +17,10 @@ if "%release%"=="1" set debug=0 && echo [release mode]
 if "%~1"==""                     echo [default mode, assuming `cardiograph` build]
 if "%~1"=="release" if "%~2"=="" echo [default mode, assuming `cardiograph` build]
 
-:: --- Unpack Command Line Build Arguments ------------------------------------
-set auto_compile_flags=
-if "%telemetry%"=="1" set auto_compile_flags=%auto_compile_flags% -DPROFILE_TELEMETRY=1 && echo [telemetry profiling enabled]
-if "%asan%"=="1"      set auto_compile_flags=%auto_compile_flags% -fsanitize=address && echo [asan enabled]
-
 :: --- Compile/Link Line Definitions ------------------------------------------
 set compile_common=     /I..\src\ /I%vendor_root%\include /nologo /FC /Z7
-set compile_debug=      call cl /Od /Ob1 /DBUILD_DEBUG=1 %compile_common% %auto_compile_flags%
-set compile_release=    call cl /O2 /DBUILD_DEBUG=0 %compile_common% %auto_compile_flags%
+set compile_debug=      call cl /Od /Ob1 /DBUILD_DEBUG=1 %compile_common%
+set compile_release=    call cl /O2 /DBUILD_DEBUG=0 %compile_common%
 set compile_link=       /link /MANIFEST:EMBED /INCREMENTAL:NO /pdbaltpath:%%%%_PDB%%%% /SUBSYSTEM:CONSOLE %vendor_root%\lib\raylib.lib gdi32.lib User32.lib shell32.lib  winmm.lib
 set out=        /out:
 
