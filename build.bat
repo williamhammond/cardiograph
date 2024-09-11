f2728960be76529d877b3d563f2083d7bd2ca966
William Hammond <william.t.hammond@gmail.com>
2024-09-10 22:45:32 -0400

    Simplify build.bat

--- build.bat
+++ build.bat
@@ -11,14 +11,11 @@ echo vendor root is %vendor_root%
 
 :: --- Unpack Arguments -------------------------------------------------------
 for %%a in (%*) do set "%%a=1"
-if not "%msvc%"=="1" if not "%clang%"=="1" set msvc=1
 if not "%release%"=="1" set debug=1
 if "%debug%"=="1"   set release=0 && echo [debug mode]
 if "%release%"=="1" set debug=0 && echo [release mode]
-if "%msvc%"=="1"    set clang=0 && echo [msvc compile]
-if "%clang%"=="1"   set msvc=0 && echo [clang compile]
-if "%~1"==""                     echo [default mode, assuming `cardiograph` build] && set raddbg=1
-if "%~1"=="release" if "%~2"=="" echo [default mode, assuming `cardiograph` build] && set raddbg=1
+if "%~1"==""                     echo [default mode, assuming `cardiograph` build]
+if "%~1"=="release" if "%~2"=="" echo [default mode, assuming `cardiograph` build]
 
 :: --- Unpack Command Line Build Arguments ------------------------------------
 set auto_compile_flags=
@@ -26,51 +23,23 @@ if "%telemetry%"=="1" set auto_compile_flags=%auto_compile_flags% -DPROFILE_TELE
 if "%asan%"=="1"      set auto_compile_flags=%auto_compile_flags% -fsanitize=address && echo [asan enabled]
 
 :: --- Compile/Link Line Definitions ------------------------------------------
-set cl_common=     /I..\src\ /I%vendor_root%\include /nologo /FC /Z7
-set clang_common=  -I..\src\ -I%vendor_root%\include -gcodeview -fdiagnostics-absolute-paths -Wall -Wno-unknown-warning-option -Wno-missing-braces -Wno-unused-function -Wno-writable-strings -Wno-unused-value -Wno-unused-variable -Wno-unused-local-typedef -Wno-deprecated-register -Wno-deprecated-declarations -Wno-unused-but-set-variable -Wno-single-bit-bitfield-constant-conversion -Wno-compare-distinct-pointer-types -Wno-initializer-overrides -Wno-incompatible-pointer-types-discards-qualifiers -Xclang -flto-visibility-public-std -D_USE_MATH_DEFINES -Dstrdup=_strdup -Dgnu_printf=printf -ferror-limit=10000
-set cl_debug=      call cl /Od /Ob1 /DBUILD_DEBUG=1 %cl_common% %auto_compile_flags%
-set cl_release=    call cl /O2 /DBUILD_DEBUG=0 %cl_common% %auto_compile_flags%
-set clang_debug=   call clang -g -O0 -DBUILD_DEBUG=1 %clang_common% %auto_compile_flags%
-set clang_release= call clang -g -O2 -DBUILD_DEBUG=0 %clang_common% %auto_compile_flags%
-set cl_link=       /link /MANIFEST:EMBED /INCREMENTAL:NO /pdbaltpath:%%%%_PDB%%%% /SUBSYSTEM:CONSOLE %vendor_root%\lib\raylib.lib gdi32.lib User32.lib shell32.lib  winmm.lib
-set clang_link=    -fuse-ld=lld -Xlinker /MANIFEST:EMBED -Xlinker /pdbaltpath:%%%%_PDB%%%%
-set cl_out=        /out:
-set clang_out=     -o
-
-:: --- Per-Build Settings -----------------------------------------------------
-set link_dll=-DLL
-if "%msvc%"=="1"    set only_compile=/c
-if "%clang%"=="1"   set only_compile=-c
-if "%msvc%"=="1"    set EHsc=/EHsc
-if "%clang%"=="1"   set EHsc=
-if "%msvc%"=="1"    set no_aslr=/DYNAMICBASE:NO
-if "%clang%"=="1"   set no_aslr=-Wl,/DYNAMICBASE:NO
-if "%msvc%"=="1"    set rc=call rc
-if "%clang%"=="1"   set rc=call llvm-rc
+set compile_common=     /I..\src\ /I%vendor_root%\include /nologo /FC /Z7
+set compile_debug=      call cl /Od /Ob1 /DBUILD_DEBUG=1 %compile_common% %auto_compile_flags%
+set compile_release=    call cl /O2 /DBUILD_DEBUG=0 %compile_common% %auto_compile_flags%
+set compile_link=       /link /MANIFEST:EMBED /INCREMENTAL:NO /pdbaltpath:%%%%_PDB%%%% /SUBSYSTEM:CONSOLE %vendor_root%\lib\raylib.lib gdi32.lib User32.lib shell32.lib  winmm.lib
+set out=        /out:
 
 :: --- Choose Compile/Link Lines ----------------------------------------------
-if "%msvc%"=="1"      set compile_debug=%cl_debug%
-if "%msvc%"=="1"      set compile_release=%cl_release%
-if "%msvc%"=="1"      set compile_link=%cl_link%
-if "%msvc%"=="1"      set out=%cl_out%
-if "%clang%"=="1"     set compile_debug=%clang_debug%
-if "%clang%"=="1"     set compile_release=%clang_release%
-if "%clang%"=="1"     set compile_link=%clang_link%
-if "%clang%"=="1"     set out=%clang_out%
 if "%debug%"=="1"     set compile=%compile_debug%
 if "%release%"=="1"   set compile=%compile_release%
 
 :: --- Prep Directories -------------------------------------------------------
 if not exist build mkdir build
-if not exist local mkdir local
-
-:: --- Get Current Git Commit Id ----------------------------------------------
-for /f %%i in ('call git describe --always --dirty') do set compile=%compile% -DBUILD_GIT_HASH=\"%%i\"
 
 :: --- Build Everything (@build_targets) --------------------------------------
 pushd build
 echo %compile% ..\src\main.cpp %compile_link% %out%cardiograph.exe 
-if "%cardiograph%"=="1" set didbuild=1 && %compile% ..\src\main.cpp %compile_link% %out%cardiograph.exe || exit /b 1
+%compile% ..\src\main.cpp %compile_link% %out%cardiograph.exe || exit /b 1
 popd
 
 :: --- Unset ------------------------------------------------------------------
@@ -79,12 +48,6 @@ set cardiograph=
 set compile=
 set compile_link=
 set out=
-set msvc=
 set debug=
 set release=
 
-:: --- Warn On No Builds ------------------------------------------------------
-if "%didbuild%"=="" (
-  echo [WARNING] no valid build target specified; must use build target names as arguments to this script, like `build cardiograph`.
-  exit /b 1
-)
